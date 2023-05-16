8f87c226ddc3d9b15bb9ee99531d758d96acddce
kilaposhi <kilapang@gmail.com>
2023-05-16 17:28:57 +0200

    up

--- src/deck/DeckFactory.cpp
+++ src/deck/DeckFactory.cpp
@@ -12,6 +12,7 @@ Deck DeckFactory::build() {
     return newDeck;
 }
 
+
 void DeckFactory::create_valued_cards(){
     cards_.reserve(number_cards_);
     for (auto color : CardColors)
@@ -19,16 +20,20 @@ void DeckFactory::create_valued_cards(){
             cards_.push_back(make_unique<ValuedCard>(value, color));
 }
 
+
 Deck DeckFactory::createClanDeck() {
     const int MAX_CLAN_CARD_VALUE = 9;
     const int MIN_CLAN_CARD_VALUE = 1;
     this->setRangeValueCard(MIN_CLAN_CARD_VALUE, MAX_CLAN_CARD_VALUE);
+    this->setNumberColors(6);
     this->create_valued_cards();
 
     return this->build();
 }
 
+
 void DeckFactory::setTypeCards(DeckType deckType) { this->deckType_ = deckType; }
+
 
 void DeckFactory::setRangeValueCard(unsigned int min_card_value, unsigned int max_card_value) {
     setTypeCards(DeckType::ValuedCard);
@@ -37,10 +42,13 @@ void DeckFactory::setRangeValueCard(unsigned int min_card_value, unsigned int ma
     this->number_cards_ = compute_number_cards(min_card_value, max_card_value, number_colors_);
 }
 
+
 void DeckFactory::setNumberColors(unsigned int number_colors) {
+    setTypeCards(DeckType::ValuedCard);
     this->number_colors_ = number_colors;
     this->number_cards_ = compute_number_cards(min_card_value_, max_card_value_, number_colors_);
 }
+
 
 unsigned int compute_number_cards(unsigned int min_value, unsigned int max_value, unsigned int num_colors){
     unsigned int number_values = max_value - min_value + 1;
