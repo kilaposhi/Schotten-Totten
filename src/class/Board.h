278ac932d2aec0c568caa00ec6c1a42d55a5d9b5
capucinebrt <capucine.berthet@etu.utc.fr>
2023-05-17 19:51:12 +0200

    début de Board

---
+++ src/class/Board.h
@@ -0,0 +1,17 @@
+
+
+#ifndef SHOTTEN_TOTTEN_BOARD_H
+#define SHOTTEN_TOTTEN_BOARD_H
+
+class Board {
+private :
+    const int numberBorder = 9;
+    const list<Border> borders;
+    Player* winner;
+public :
+    const int getNumberBorder() const {return numberBorder};
+    Player* getWinner() const {return winner};
+    const list<Border> getBorders() const {return borders};
+
+};
+#endif //SHOTTEN_TOTTEN_BOARD_H
