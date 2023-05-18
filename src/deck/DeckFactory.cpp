54b6ddea4013dcd7aedd312d15a034623475ab38
kilaposhi <105288831+kilaposhi@users.noreply.github.com>
2023-05-18 16:55:59 +0200

    Deck factory (#12)
    * okay2
    
    * vector<Card>
    
    * refactor DeckBuilder -> DeckFactory
    
    * uml Factory
    
    * fix main
    
    * delete files
    
    * private DeckFactory
    
    * up
    
    * clean main

---
+++ src/deck/DeckFactory.cpp
@@ -0,0 +1,56 @@
+#include "DeckFactory.h"
+
+Deck DeckFactory::build() {
+    DeckInfo newDeckInfo(
+            number_cards_,
+            min_card_value_,
+            max_card_value_
+            );
+    Deck newDeck;
+    newDeck.deckInfo_ = newDeckInfo;
+    copy_vector_cards(this->cards_, newDeck.cards_);
+    return newDeck;
+}
+
+
+void DeckFactory::create_valued_cards(){
+    cards_.reserve(number_cards_);
+    for (auto color : CardColors)
+        for (int value = min_card_value_; value <= max_card_value_; value++)
+            cards_.push_back(make_unique<ValuedCard>(value, color));
+}
+
+
+Deck DeckFactory::createClanDeck() {
+    const int MAX_CLAN_CARD_VALUE = 9;
+    const int MIN_CLAN_CARD_VALUE = 1;
+    this->setRangeValueCard(MIN_CLAN_CARD_VALUE, MAX_CLAN_CARD_VALUE);
+    this->setNumberColors(6);
+    this->create_valued_cards();
+
+    return this->build();
+}
+
+
+void DeckFactory::setTypeCards(DeckType deckType) { this->deckType_ = deckType; }
+
+
+void DeckFactory::setRangeValueCard(unsigned int min_card_value, unsigned int max_card_value) {
+    setTypeCards(DeckType::ValuedCard);
+    this->min_card_value_ = min_card_value;
+    this->max_card_value_ = max_card_value;
+    this->number_cards_ = compute_number_cards(min_card_value, max_card_value, number_colors_);
+}
+
+
+void DeckFactory::setNumberColors(unsigned int number_colors) {
+    setTypeCards(DeckType::ValuedCard);
+    this->number_colors_ = number_colors;
+    this->number_cards_ = compute_number_cards(min_card_value_, max_card_value_, number_colors_);
+}
+
+
+unsigned int compute_number_cards(unsigned int min_value, unsigned int max_value, unsigned int num_colors){
+    unsigned int number_values = max_value - min_value + 1;
+    return number_values *  num_colors;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,22 @@
 int main() {
 
     //-----launch_Schotten_Totten1:
-    const int MAX_CLAN_CARD_STRENGTH = 9;
-    const int MIN_CLAN_CARD_STRENGTH = 1;
-    const int NUMBER_CLAN_CARDS = 54;
 
     unique_ptr<Card> card1 = make_unique<ValuedCard>(2, CardColor::orange);
     cout << *card1 << '\n' << '\n';
 
-    DeckBuilder deckBuilder;
-    Deck deck = deckBuilder.createClanDeck().build();
-    deck.shuffle();
+    Deck clanDeck = DeckFactory().createClanDeck();
+    clanDeck.shuffle();
 
 
 
     Deck discardDeck;
     for (int i = 0; i < 15; ++i)
-        discardDeck.putCard(deck.drawCard());
+        discardDeck.putCard(clanDeck.drawCard());
     discardDeck.print();
     cout << '\n';
 
-    deck.print();
+    clanDeck.print();
     return 0;
 }
 
