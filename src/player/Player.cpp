cd09bf04fa511f3a44e49f17f7fd6aa31efd3d8b
Martin Halbourg <halbourg.martin@gmail.com>
2023-05-21 13:41:04 +0200

    Commit Player.cpp and Player.h

--- src/player/Player.cpp
+++ src/player/Player.cpp
@@ -12,20 +12,20 @@ class Border;
 class Deck;
 
 
-Player::Player(Player* p, int max_card_) : player(p), hand{}, max_cards(), claimed_borders_vector{} {}
+Player::Player(int id_, Player* p, int max_card_) : id(id_), player(p), hand{}, max_cards(), claimed_borders{} {}
 
-void Player::add_card_into_hand(unique_ptr<Card> card_) {
+void Player::add_card_into_hand(std::unique_ptr<Card>  card_) {
     if (hand.size() == max_cards) {
         throw PlayerException("The hand is full");
     }
     hand.push_back(std::move(card_));
 }
 
-unique_ptr<Card> Player::remove_card_from_hand(int card_index) {
+std::unique_ptr<Card>  Player::remove_card_from_hand(int card_index) {
     if (hand.empty()) {
         throw PlayerException("The hand is empty");
     }
-    if (card_index > hand.size()) {
+    if (card_index == hand.size()) {
         throw PlayerException("Index not found in hand");
     }
     std::unique_ptr<Card> returned_card = std::move(hand[card_index]);
@@ -33,36 +33,30 @@ unique_ptr<Card> Player::remove_card_from_hand(int card_index) {
     return returned_card;
 }
 
-void Player::play_valued_card(int card_index, Border& border_) {
-    std::unique_ptr<Card> played_card = remove_card_from_hand(card_index);
-    border_.addValueCard(std::move(played_card), id);
+void Player::play_card(int card_index, Border& border_) {
+    auto played_card = remove_card_from_hand(card_index);
+    border_.addCard(std::move(played_card), player);
 }
-
-void Player::play_tactical_card(int card_index, Border& border_){
-    std::unique_ptr<Card> played_card = remove_card_from_hand(card_index);
-    border_.addTacticalCard(std::move(played_card), id);
-}
-
 
 void Player::draw_card(Deck deck_) {
     auto drawn_card = deck_.drawCard();
     add_card_into_hand(std::move(drawn_card));
 }
 
-void Player::claim_borders_vector(Border& border_){
-    unsigned int border_num = border_.getSlotNumber();
+void Player::claim_borders(Border& border_){
+    int border_num = border_.getSlotNumber();
     border_.setClaimed(id);
     if (border_num > 9) {                                   // Remarque 1
         throw std::out_of_range("The border index > 9");
     }
-    claimed_borders_vector.push_back(border_num);
+    claimed_borders.push_back(border_num);
 
 }
-vector<int> Player::getClaimed_borders_vector(){
+vector<int> Player::getClaimed_borders(){
     vector<int> tab_of_claimed_borders;
     int count = 0;
-    for (int borders : claimed_borders_vector) {
-        tab_of_claimed_borders.push_back(claimed_borders_vector[count]);
+    for (int borders : claimed_borders) {
+        tab_of_claimed_borders.push_back(claimed_borders[count]);
         count++;
     }
     return tab_of_claimed_borders;
@@ -70,6 +64,16 @@ vector<int> Player::getClaimed_borders_vector(){
 }
 
 int Player::getNumber_of_cards() const{return hand.size();}
+
+int Player::getId(Player* player){
+    if (player != nullptr) {
+        return player->id;
+    }
+    else {
+        throw PlayerException("player != nullptr");
+    }
+}
+
 
 
 
--- src/player/Player.h
+++ src/player/Player.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <list>
 #include <string>
+#include <variant>
 
 #include "deck/Card.h"
 #include "board/Border.h"
@@ -12,7 +13,7 @@
 
 using std::vector, std::list, std::array, std::string;
 
-class Stone;
+class Border;
 class Deck;
 
 class PlayerException : public std::exception {
@@ -35,23 +36,24 @@ private:
      Player* player;
      vector<unique_ptr<Card>> hand;
      int max_cards;
-     vector<unsigned int> claimed_borders_vector;
+     vector<int> claimed_borders;
 
 public:
 
     ~Player()=default;
-    Player(Player* p, int max_cards_);
+    Player(int id_,  Player* p, int max_cards_);
 
     Player& operator = (const Player&) = delete;
+    Player(const Player&) = delete;
 
-    void add_card_into_hand(unique_ptr<Card> card_);
-    unique_ptr<Card> remove_card_from_hand(int card_index);
-    void play_valued_card(int card_index, Border& border_);
-    void play_tactical_card(int card_index, Border& border_);
+    void add_card_into_hand(std::unique_ptr<Card>  card_);
+    std::unique_ptr<Card>  remove_card_from_hand(int card_index);
+    void play_card(int card_index, Border& border_);
     void draw_card(Deck deck_);
-    void claim_borders_vector(Border& border_);
-    vector<int> getClaimed_borders_vector();
+    void claim_borders(Border& border_);
+    vector<int> getClaimed_borders();
     [[nodiscard]] int getNumber_of_cards() const;
+    int static getId(Player* player);
 
 };
 
