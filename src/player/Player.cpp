a1ef8eefa5dafb5d99da9ab65f7450e7499c02e7
MartinH02 <halbourg.martin@gmail.com>
2023-06-09 18:09:36 +0200

    Ajout de play_card()
    ATTENTION y a pas défausse

--- src/player/Player.cpp
+++ src/player/Player.cpp
@@ -64,12 +64,35 @@ std::unique_ptr<Card>  Player::remove_card_from_hand(int card_index) {
     return returned_card;
 }
 
-/*
-void Player::play_card(int card_index, Border& border_) {
-    auto played_card = remove_card_from_hand(card_index);
-    border_.addCard(std::move(played_card), player);
+void Player::play_card(int card_index, Border& border) {
+    if (card_index < 0 || card_index >= hand.size()) {
+        throw PlayerException("Invalid card index");
+    }
+
+    std::unique_ptr<Card> card = remove_card_from_hand(card_index);
+
+    if (auto valued_card = dynamic_cast<ValuedCard*>(card.get())) {
+        std::unique_ptr<ValuedCard> unique_valued_card = std::make_unique<ValuedCard>(*valued_card);
+        border.addValueCard(std::move(unique_valued_card), this);
+    } else if (auto tactic_card = dynamic_cast<TacticCard*>(card.get())) {
+        TacticType type = tactic_card->getName();
+        if (type == TacticType::joker || type == TacticType::spy || type == TacticType::shield_bearer ||
+            type == TacticType::blind_man_bluff || type == TacticType::mud_fight) {
+            std::unique_ptr<TacticCard> unique_tactic_card(tactic_card);
+            border.addTacticalCard(std::move(unique_tactic_card), this);
+        } else if (type == TacticType::recruiter || type == TacticType::strategist ||
+                   type == TacticType::banshee || type == TacticType::traiter) {
+            // Ajouter la carte à la défausse
+            // defausse.push_back(std::move(card)); --> où est défausse
+        } else {
+            // Type de carte tactique non pris en charge, vous pouvez gérer l'erreur en conséquence
+            throw PlayerException("Unsupported tactic card type");
+        }
+    } else {
+        // Type de carte non pris en charge, vous pouvez gérer l'erreur en conséquence
+        throw PlayerException("Unsupported card type");
+    }
 }
-*/
 /*
 void Player::draw_card(Deck deck_) {
     auto drawn_card = deck_.drawCard();
