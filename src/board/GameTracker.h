ac281dbf6e6c3afb3cf9dd51e5a4f2ef20059794
lilianvln <122017591+lilianvln@users.noreply.github.com>
2023-06-09 19:00:43 +0200

    Update GameTracker.h

--- src/board/GameTracker.h
+++ src/board/GameTracker.h
@@ -1,14 +1,23 @@
-//
-// Created by tassili on 18/05/2023.
-//
-
 #ifndef SCHOTTEN_TOTTEN_GAMETRACKER_H
 #define SCHOTTEN_TOTTEN_GAMETRACKER_H
+
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <string>
+
+#include "deck/Card.h"
+#include "player/Player.h"
+#include "board/Combination.h"
+#include "deck/Deck.h"
+
+
 // Classe abstraite pour les observateurs
 class Observer {
 public:
     virtual void update() = 0;
 };
+
 
 // Classe GameTracker qui est l'observateur
 class GameTracker : public Observer {
@@ -17,12 +26,11 @@ private:
     Deck playedCardsDeck;
 
 public:
-    void update() override {
-        // Mettre à jour les decks lorsque la carte est jouée
-        // à partir de la classe Stone
-        playedCardsDeck.putCard(remainingCardsDeck.drawCard());
-    }
+    void update() override;
+    void copyDeck(Deck tacticDeck, Deck clanDeck);
+    void transferCard();
 };
+
 
 
 
